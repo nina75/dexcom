0e3bd14eeb0aa5d2a4e7641794b523a56d983da2
b0r1slav <borislav.madzharov@gmail.com>
2022-07-26 20:17:39 +0300

    fix bs modal not working on mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -161,9 +161,18 @@ $(function () {
       }
     }
 
-    Notification.requestPermission().then(function (permission) {
-      console.log('Notifications: ' + permission);
-    });
+    //Ask for notifications permission
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      console.log('Notifications granted');
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          console.log('Notifications granted');
+        }
+      });
+    }
     
     function showNotification(sgvData = {}) {
       const mmol = sgvData.sgv / 18;
